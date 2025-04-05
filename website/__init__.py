7af72ec4f7cd3189feb1c77c9caebaf719e716f6
Dillon Anawalt <djanawalt@dons.usfca.edu>
2025-04-04 22:00:58 -0700

    sum changes.

--- website/__init__.py
+++ website/__init__.py
@@ -3,5 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'thisisthekey'
+    from .views import views
+    from .auth import auth
 
+    app.register_blueprint(views, url__prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     return app
